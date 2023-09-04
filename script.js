5d2d29fd8b316947d5fa07836dc5224818acbd6a
bricioo0 <briciopincha@gmail.com>
2023-09-04 18:27:51 -0300

    update 7

---
+++ script.js
@@ -0,0 +1,6 @@
+const botonCambiar = document.getElementById('cambiar');
+const elementotexto = document.getElementById('Texto')
+const body = document.getElementById('bodys')
+
+
+botonCambiar.addEventListener('click', () =>  elementotexto.textContent = 'sos hincha de gimnasia mal ahi amigo')
