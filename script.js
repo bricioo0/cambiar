29ad997c0cf95be31dd0279a358e86384d9c18a2
bricioo0 <briciopincha@gmail.com>
2023-09-07 14:53:47 -0300

    update 13

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ const botonCambiar = document.getElementById('cambiar');
 const elementoTexto = document.getElementById('Texto');
 
 botonCambiar.addEventListener('click', () => {
-    // Genera colores aleatorios para el texto y el fondo
+
     const colorTexto = generarColorAleatorio();
     const colorFondo = generarColorAleatorio();
 
-    // Cambia el texto y el fondo con los colores aleatorios
+   
     elementoTexto.style.color = colorTexto;
     document.body.style.backgroundColor = colorFondo;
 });
 
-// Función para generar un color aleatorio en formato hexadecimal
+
 function generarColorAleatorio() {
     const letrasHex = '0123456789ABCDEF';
     let color = '#';
