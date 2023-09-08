5dd4767b57b361f6c065d9060e82398c77b4007c
bricioo0 <briciopincha@gmail.com>
2023-09-08 15:38:26 -0300

    update 17

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const botonCambiar = document.getElementById('cambiar');
 const elementoTexto = document.getElementById('Texto');
+const elementoTitulo = document.getElementById('Titulo')
+const elementoSubtitulo = document.getElementById('Subtitulo')
+
 
 botonCambiar.addEventListener('click', () => {
 
     const colorTexto = generarColorAleatorio();
     const colorFondo = generarColorAleatorio();
     const colorBoton = generarColorAleatorio();
-    
+    const colorTitulo = generarColorAleatorio();
+    const colorSubtitulo = generarColorAleatorio();
+
     elementoTexto.style.color = colorTexto;
     document.body.style.backgroundColor = colorFondo;
     botonCambiar.style.backgroundColor = colorBoton;
+    elementoTitulo.style.color = colorTitulo;
+    elementoSubtitulo.style.color = colorSubtitulo
 });
 
 
